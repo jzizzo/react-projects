75250de71019d32370d32f97ed42d67a8356455c
Joseph Zizzo <hire.jzizzo@gmail.com>
2018-12-02 05:56:19 -0800

    Add functional component project

--- components/src/index.js
+++ components/src/index.js
@@ -1,8 +1,40 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import faker from "faker";
+import CommentDetail from './CommentDetail.js';
+import ApprovalCard from './ApprovalCard.js';
 
 const App = () => {
-  return <div>Hi There!</div>;
+  return (
+    <div className="ui container comments">
+      <ApprovalCard>
+        <CommentDetail
+          author="Ahmed"
+          timeAgo="Today at 10:05AM"
+          avatar={faker.image.avatar()}
+          content="Nice blog!"
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Rocky"
+          timeAgo="Today at 1PM"
+          avatar={faker.image.avatar()}
+          content="You did it!"
+        />
+      </ApprovalCard>
+       
+      <ApprovalCard>
+        <CommentDetail
+          author="Dillon"
+          timeAgo="Today at 5:45PM"
+          avatar={faker.image.avatar()}
+          content="You're the best!"
+        />
+      </ApprovalCard>
+    </div>
+  );
 };
 
-ReactDom.render(<App />, document.querySelector('#root'));
+ReactDom.render(<App />, document.querySelector("#root"));
